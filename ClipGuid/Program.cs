6014dc1ad3c7dd4cd8466efff07afcdfd70e6639
Javohir0102 <javohiranvarov96@gmail.com>
2024-07-01 21:18:37 +0500

    Add project files.

---
+++ ClipGuid/Program.cs
@@ -0,0 +1,24 @@
+﻿using TextCopy;
+using System;
+
+namespace ClipGuid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection = "";
+            do
+            {
+                string stringFieldGuid = Guid.NewGuid().ToString();
+                Console.WriteLine(stringFieldGuid);
+                TextCopy.ClipboardService.SetText(stringFieldGuid);
+                Console.WriteLine("Copied");
+                
+                Console.WriteLine("Do you want again NewGuid? press yes");
+                selection = Console.ReadLine();
+            } 
+            while (selection == "yes");
+        }
+    }
+}
